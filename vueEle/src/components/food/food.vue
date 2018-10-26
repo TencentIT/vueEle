912a45bd226edf816f81b0bcf384858c910a0053
TencentIT <1969368719@qq.com>
2018-10-26 21:17:23 +0800

    10.26 food.vue组件静态头部写完了

--- vueEle/src/components/food/food.vue
+++ vueEle/src/components/food/food.vue
@@ -3,9 +3,21 @@
     <div class="food" v-show="showFlag">
     <div class="food-content">
       <div class="image-header">
-        <img src="http://fuss10.elemecdn.com/c/cd/c12745ed8a5171e13b427dbc39401jpeg.jpeg?imageView2/1/w/750/h/750">
-        <div class="black" @click="hide"></div>
-          <i class="i-arrow_lift"></i>
+        <img :src="food.image">
+        <div class="black" @click="hide">
+          <i class="icon-arrow_lift"></i>
+        </div>
+      </div>
+      <div class="content">
+        <h1 class="title">{{food.name}}</h1>
+        <div class="detail">
+          <span class="sell-count">月售{{food.sellCount}}份</span>
+          <span class="rating">好评率{{food.rating}}%</span>
+        </div>
+        <div class="price">
+           <span class="now">￥{{food.price}}</span>
+           <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+        </div>
       </div>
     </div>
   </div>
@@ -13,11 +25,11 @@
 </template>
 <script>
 export default {
-  // props: {
-  //   food: {
-  //     type: Object
-  //   }
-  // },
+  props: {
+    food: {
+      type: Object
+    }
+  },
   data() {
     return {
       showFlag: false
@@ -25,7 +37,7 @@ export default {
   },
   methods: {
     hide() {
-      console.log(1);
+      this.showFlag = false;
     },
     show() {
       this.showFlag = true;
@@ -54,11 +66,51 @@ export default {
     position relative
     width 100% 
     height 0 
+    padding-top 100%
     img
       position absolute
       top  0
       left 0
       width 100% 
       height 100%
-    // .black
+    .black
+      position absolute
+      top: 10px
+      left: 0
+      .icon-arrow_lift
+        display block
+        padding 10px
+        font-size 20px
+        color #fff
+  
+  .content
+    position relative
+    padding: 18px
+    .title
+      line-height: 14px
+      margin-bottom: 8px
+      font-size: 14px
+      font-weight: 700
+      color: rgb(7, 17, 27)
+    .detail
+      margin-bottom: 18px
+      line-height: 10px
+      height: 10px
+      font-size: 0
+      .sell-count
+        font-size: 10px
+        color: rgb(147, 153, 159)
+      .sell-count
+        margin-right: 12px
+    .price
+      font-weight: 700
+      line-height: 24px
+      .now
+        margin-right: 8px
+        font-size: 14px
+        color: rgb(240, 20, 20)
+      .old
+        text-decoration: line-through
+        font-size: 10px
+        color: rgb(147, 153, 159)
 </style>
